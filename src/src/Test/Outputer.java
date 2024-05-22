90fe0ccc0d4dd3af1d6788d01f12dc61f1f2084c
Jackson Kettel <69734811+Kettelcorn@users.noreply.github.com>
2024-05-22 12:00:15 -0700

    Created outputer class and outlined methods

---
+++ src/src/Test/Outputer.java
@@ -0,0 +1,14 @@
+package Test;
+
+public class Outputer {
+    private AlgoResult[] results;
+
+
+    public Outputer (Executer executer) {
+        results = executer.getResults();
+    }
+
+    public void printResults() {
+
+    }
+}
