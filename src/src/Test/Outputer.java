054d76a57632c28a5b032ff1ee993f252b83bd92
Jackson Kettel <69734811+Kettelcorn@users.noreply.github.com>
2024-05-22 12:16:15 -0700

    Added methods for calculating results

--- src/src/Test/Outputer.java
+++ src/src/Test/Outputer.java
@@ -1,5 +1,7 @@
 package Test;
 
+import java.util.Map;
+
 public class Outputer {
     private AlgoResult[] results;
 
@@ -10,5 +12,37 @@ public class Outputer {
 
     public void printResults() {
 
+    }
+
+    public int getTotalTime() {
+
+    }
+
+    public double getCPUUtilization() {
+
+    }
+
+    public Map<String, Integer> getWaitingTimes() {
+
+    }
+
+    public double getAverageWaitingTime() {
+
+    }
+
+    public Map<String, Integer> getTurnaroundTimes() {
+
+    }
+
+    public double getAverageTurnaroundTime() {
+
+    }
+
+    public Map<String, Integer> getResponseTimes() {
+
+    }
+
+    public double getAverageResponseTime() {
+        
     }
 }
