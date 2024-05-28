651c992b31b0a2a5c465db9a5667ad7a544a1d29
Tamara <terratamara@yahoo.com>
2024-05-28 14:35:36 -0700

    Merge conflict prevention

---
+++ src/src/JUnitTesting/AlgoTest.java
@@ -0,0 +1,19 @@
+package Algorithms;
+
+import org.junit.Test;
+
+class AlgoTest{
+    @Test
+    public void testRR(){
+
+    }
+
+    public void testFCFSNonPre(){
+
+    }
+
+    public void testMFQ(){
+
+    }
+
+}
