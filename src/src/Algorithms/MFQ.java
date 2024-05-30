38704bf8bdc0266ae1e0e13cac5bea04dfd9b408
Jackson Kettel <69734811+Kettelcorn@users.noreply.github.com>
2024-05-29 20:35:34 -0700

    Implemented MFQ algo

--- src/src/Algorithms/MFQ.java
+++ src/src/Algorithms/MFQ.java
@@ -4,19 +4,110 @@ import Test.AlgoResult;
 import Test.SProcess;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.Queue;
 
 public class MFQ implements SchedulerAlgorithm{
 
     AlgoResult result;
+
+    public MFQ() {
+        this.result = new AlgoResult(getName());
+    }
     /**
      * Method to run a list of processes through a scheduling algorithm
      *
      * @param processes List of processes
      * @return An AlgoResult object, a summary of the findings
      */
-    @Override
+
     public AlgoResult runAlgo(ArrayList<SProcess> processes) {
-        return null;
+        int currentTime = 0;
+
+        Queue<SProcess> queue1 = new LinkedList<>(processes);
+        Queue<SProcess> queue2 = new LinkedList<>();
+        Queue<SProcess> queue3 = new LinkedList<>();
+        Queue<SProcess> ioQueue = new LinkedList<>();
+
+        while (!queue1.isEmpty() || !queue2.isEmpty() || !queue3.isEmpty() || !ioQueue.isEmpty()) {
+            handleIOQueue(ioQueue, queue1, currentTime);
+
+            if (!queue1.isEmpty()) {
+                SProcess currentProcess = queue1.poll();
+                int executedTime = executeProcess(currentProcess, 5, currentTime);
+                currentTime += executedTime;
+                if (currentProcess.getBurstTimes()[currentProcess.getCurrCPUindex()] > 0) {
+                    queue2.add(currentProcess);
+                } else {
+                    finalizeProcess(currentProcess, currentTime, ioQueue);
+                }
+            } else if (!queue2.isEmpty()) {
+                SProcess currentProcess = queue2.poll();
+                int executedTime = executeProcess(currentProcess, 10, currentTime);
+                currentTime += executedTime;
+                if (currentProcess.getBurstTimes()[currentProcess.getCurrCPUindex()] > 0) {
+                    queue3.add(currentProcess);
+                } else {
+                    finalizeProcess(currentProcess, currentTime, ioQueue);
+                }
+            } else if (!queue3.isEmpty()){
+                SProcess currentProcess = queue3.poll();
+                int executedTime = executeProcess(currentProcess, Integer.MAX_VALUE, currentTime);
+                currentTime += executedTime;
+                finalizeProcess(currentProcess, currentTime, ioQueue);
+            } else {
+                if (!ioQueue.isEmpty()) {
+                    int minReturnTime = Integer.MAX_VALUE;
+                    for (SProcess process : ioQueue) {
+                        if (process.getReturnTime() < minReturnTime) {
+                            minReturnTime = process.getReturnTime();
+                        }
+                    }
+                    currentTime = minReturnTime;
+                    handleIOQueue(ioQueue, queue1, currentTime);
+                } else {
+                    currentTime++;
+                }
+            }
+        }
+        return result;
+    }
+
+    private void handleIOQueue(Queue<SProcess> ioQueue, Queue<SProcess> queue1, int currentTime) {
+        Queue<SProcess> tempQueue = new LinkedList<>();
+        while (!ioQueue.isEmpty()) {
+            SProcess currentProcess = ioQueue.poll();
+            if (currentProcess.getReturnTime() <= currentTime) {
+                queue1.add(currentProcess);
+            } else {
+                tempQueue.add(currentProcess);
+            }
+        }
+        ioQueue.addAll(tempQueue);
+    }
+
+    private int executeProcess(SProcess process, int quantum, int currentTime) {
+        int remainingBurst = process.getBurstTimes()[process.getCurrCPUindex()];
+        int executedTime = Math.min(remainingBurst, quantum);
+        process.setStartTime(currentTime);
+        process.setStopTime(currentTime + executedTime);
+        result.getCPUactivity().add(new AlgoResult.Pair(process.getStartTime(), process.getStopTime()));
+        result.getExecutionOrder().add(process.getTitle());
+        process.getBurstTimes()[process.getCurrCPUindex()] -= executedTime;
+        return executedTime;
+    }
+
+    private void finalizeProcess(SProcess process, int currentTime, Queue<SProcess> ioQueue){
+        if (process.getCurrCPUindex() >= process.getBurstTimes().length - 1) {
+            process.setExitTime(currentTime);
+        } else {
+            int ioBurst = process.getBurstTimes()[process.getCurrIOindex()];
+            process.setReturnTime(currentTime + ioBurst);
+            process.setCurrCPUindex(process.getCurrCPUindex() + 2);
+            process.setCurrIOindex(process.getCurrIOindex() + 2);
+            ioQueue.add(process);
+        }
     }
 
     /**
@@ -26,13 +117,12 @@ public class MFQ implements SchedulerAlgorithm{
      */
     @Override
     public String getName() {
-        return null;
+        return "Multilevel Feedback Queue";
     }
 
     @Override
     public AlgoResult getResults(){
         return result;
-
     }
 
 }
